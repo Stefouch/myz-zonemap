5af15f0395127090ec063162931e54902978ca44
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-03-20 12:17:34 +0100

    upd

--- src/main.js
+++ src/main.js
@@ -6,7 +6,15 @@ import store from "./store";
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	data: {
+		version: "0.60",
+		versionLog: [
+			'0.60: Total revamp.',
+			'0.56: Fixed a compatibility issue with web browsers other than Chrome, which was introduced with the last fix.',
+			'0.55: Fixed various map download/upload bugs.'
+		],
+	},
+	render: h => h(App)
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,39 @@
 <template>
 <!-- MAIN CONTAINER ======================================================= -->
-<div id="main" class="container">
+<div class="bg-index">
+	<div id="main" class="container">
 
-	<div style="text-align: center;">
-		<img id="myz-logo" src="../assets/myz-title-white.png" alt="MYZ Logo">
-	</div>
+		<div style="text-align: center;">
+			<img id="myz-logo" src="../assets/myz-title-white.png" alt="MYZ Logo">
+		</div>
 
-	<h1>Find My Path</h1>
+		<h1>Find My Path</h1>
 
-	<p>Welcome fellow Mutant,
-	<br />Are you lost in the Zone? You're chased by a group of <i>Zone Ghouls</i> and you can't remember the way back to your Ark? Or maybe you are looking after Eden? Don't worry, you find the right place for help!</p>
-	<p><b>Mutant: Find My Path</b> is a web application for managing the Map of the Zone for the <i>Mutant:&nbsp;Year&nbsp;Zero</i> tabletop roleplaying game.</p>
+		<p>Welcome fellow Mutant,
+		<br />Are you lost in the Zone? You're chased by a group of <i>Zone Ghouls</i> and you can't remember the way back to your Ark? Or maybe you are looking after Eden? Don't worry, you find the right place for help!</p>
+		<p><b>Mutant: Find My Path</b> is a web application for managing the Map of the Zone for the <i>Mutant:&nbsp;Year&nbsp;Zero</i> tabletop roleplaying game.</p>
 
-	<div class="map-buttons">
-		<a id="new-map" class="btn btn-lg btn-outline-light" href="#new-map-panel"><i class="fas fa-file"></i>Create a new Map</a>
-		<a id="open-map" class="btn btn-lg btn-outline-light" href="#open-map-panel"><i class="fas fa-folder-open"></i>Open an existing Map</a>
-		<a id="previous-map" class="btn btn-lg btn-outline-light" style="display: none;" href="./zonemap.php"><i class="fas fa-road"></i>Load last Map</a>
-	</div>
+		<div class="map-buttons">
+			<a id="new-map" class="btn btn-lg btn-outline-light" href="#new-map-panel"><i class="fas fa-file"></i>Create a new Map</a>
+			<a id="open-map" class="btn btn-lg btn-outline-light" href="#open-map-panel"><i class="fas fa-folder-open"></i>Open an existing Map</a>
+			<a id="previous-map" class="btn btn-lg btn-outline-light" style="display: none;" href="./zonemap.php"><i class="fas fa-road"></i>Load last Map</a>
+		</div>
 
-	<h5>Notifications</h5>
+		<h5>Notifications</h5>
 
-	<div class="alert alert-success mb-1"><i class="fas fa-check-square"></i> Fully tested web browsers include: Google Chrome, Mozilla Firefox, and Safari (both on desktops and smartphones).</div>
+		<div class="alert alert-success mb-1"><i class="fas fa-check-square"></i> Fully tested web browsers include: Google Chrome, Mozilla Firefox, and Safari (both on desktops and smartphones).</div>
 
-	<div class="alert alert-warning mb-1"><i class="fas fa-exclamation-triangle"></i> Development in progress! Fonctionalities are subject to evolve. Bugs are not impossible. If you're experiencing some weird behavior, please report it there: <a href="http://git" target="_blank">bug report</a>.</div>
+		<div class="alert alert-warning mb-1"><i class="fas fa-exclamation-triangle"></i> Development in progress! Fonctionalities are subject to evolve. Bugs are not impossible. If you're experiencing some weird behavior, please report it there: <a href="http://git" target="_blank">bug report</a>.</div>
 
-	<div class="alert alert-danger mb-1"><i class="fas fa-times-circle"></i> Not compatible with Internet Explorer.</div>
+		<div class="alert alert-danger mb-1"><i class="fas fa-times-circle"></i> Not compatible with Internet Explorer.</div>
 
-	<h5>Build</h5>	
-	<p class="ml-4"><b><?= $zonemapVersionNumber ?></b>.<?= $zonemapVersionDate ?></p>
+		<h5>Build</h5>	
+		<p class="ml-4"><b>{{ version }}</b>.{{ Date.now() }}</p>
 
-	<h5>Version Log</h5>
-	<p>
-		<?php
-		foreach ($zonemapVersionLog as $entry) {
-			echo '<p class="ml-4">' . $entry . '</p>';
-		}
-		?>
-	</p>
+		<h5>Version Log</h5>
+		<p v-for="log in versionLog">{{ log }}</p>
 
-</div><!--/.Container-->
-
+	</div><!--/.Container-->
 </div>
 </template>
 
@@ -48,9 +42,83 @@
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+	name: "home",
+	props: ['version', 'versionLog'],
+	components: {
+		HelloWorld
+	}
 };
 </script>
+
+<style scoped>
+/* MAIN PAGE (INDEX) ======================================================= */
+#main {
+	font-family: 'Futura Std Medium'!important;
+	font-size: 1rem;
+	color: #fff!important;
+	min-height: 70vh;
+	margin-top: 6rem;
+}
+
+#main h1 {
+	font-size: calc(3.5rem + 2vw);
+	color: #fff!important;
+}
+
+/* @media (max-width: 576px) { #main h1 { font-size: 4rem; } } */
+
+#main h2, #main h3, #main h4, #main h5 {
+	font-family: 'Futura Std Heavy';
+	text-transform: uppercase;
+	margin: 1rem 0;
+}
+
+#main p {
+	font-family: 'Futura Std Medium';
+}
+
+#main b {
+	font-family: 'Futura Std Heavy';
+}
+
+#main .map-buttons {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin: 2rem;
+}
+
+@media (min-width: 992px) { #main .map-buttons { flex-direction: row; } }
+
+#main .map-buttons a {
+	font-family: 'Big Noodle Titling';
+	font-size: 1.5rem;
+	text-align: left;
+	width: 320px;
+	max-width: 320px;
+	margin: 0 1rem 1rem 1rem;
+	background-color: #19181f;
+}
+
+#main .map-buttons a:hover {
+	background-color: #fff;
+}
+
+#main .map-buttons a i {
+	min-width: 32px;
+	margin: 0 12px;
+}
+
+/* BACKGROUNDS ============================================================= */
+.bg-index {
+	background-image: url('../assets/darkback-wide.jpg');
+	background-repeat: no-repeat;
+	background-position: top center;
+	background-attachment: fixed;
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+}
+</style>
