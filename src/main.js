e7a21faec20d7bf9152b06073e65c83cc731f37a
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-03-20 16:07:08 +0100

    Added Vuetify

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Vuetify from 'vuetify';
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify);
 
 new Vue({
 	router,
