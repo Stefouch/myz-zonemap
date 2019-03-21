f9b6e7223a3e1c6dc2febb50716029b978c40c3e
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-03-21 16:31:35 +0100

    Initial commit

--- src/views/Zonemap.vue
+++ src/views/Zonemap.vue
@@ -1,9 +1,18 @@
 <template>
-<div></div>
+	<div>
+		<h1>Hello World</h1>
+		<h3>{{ zonemap }}</h3>
+	</div>
 </template>
 
 <script>
+import ZoneMap from '@/zonemap/ZoneMap';
+
 export default {
-	name: 'zonemap'
+	name: 'zonemap',
+	// props: ['zonemap'],
+	data: () => ({
+		zonemap: this.$root.zonemap
+	})
 }
 </script>
