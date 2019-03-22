d36e87914bf65f338f5c006df5888f2a8a2caf69
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-03-22 18:17:02 +0100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -14,13 +14,13 @@ export default new Router({
 			props: true
 		},
 		{
-			path: '/about',
-			name: 'about',
+			path: '/help',
+			name: 'help',
 			// route level code-splitting
 			// this generates a separate chunk (about.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
 			component: () =>
-				import(/* webpackChunkName: "about" */ './views/About.vue')
+				import(/* webpackChunkName: "about" */ './views/Help.vue')
 		},
 		{
 			path: '/zonemap',
