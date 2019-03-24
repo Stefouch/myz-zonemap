42b3d46a5fc3d479b6a91bef30876ab29130a426
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-03-24 23:15:32 +0100

    Update Zonescreen.vue

--- src/views/Zonescreen.vue
+++ src/views/Zonescreen.vue
@@ -1,44 +1,89 @@
 <template>
-	<div v-if="zonemap">
-		<v-toolbar id="navbar-myz" dense dark>
-			<v-toolbar-side-icon></v-toolbar-side-icon>
-			<v-toolbar-title class="mr-5">Find My Path{{ mapTitle }}</v-toolbar-title>
-			<v-btn icon>
-				<v-icon>mdi-content-save</v-icon>
-			</v-btn>
-			<v-btn icon @click="$router.push('help')">
-				<v-icon>mdi-help-circle-outline</v-icon>
-			</v-btn>
-			<v-spacer></v-spacer>
-			<v-btn icon @click="closeZonescreen()">
-				<v-icon>mdi-close</v-icon>
-			</v-btn>
-		</v-toolbar>
-		<v-layout column id="zonescreen">
-			<v-layout row class="zonerow" v-for="y in zonemap.height" :key="y">
-				<zm-sector
-					v-for="x in zonemap.width"
-					:key="coord(x, y)"
-					:id="coord(x, y)"
-					:gmeye="gmeye"
-					:sector="zonemap.get(coord(x, y))"
-				/>
-			</v-layout>
+<div v-if="zonemap">
+	<v-toolbar id="navbar-myz" dense dark>
+		<v-toolbar-side-icon></v-toolbar-side-icon>
+		<v-toolbar-title class="mr-5">Find My Path{{ mapTitle }}</v-toolbar-title>
+		<v-btn icon>
+			<v-icon>mdi-content-save</v-icon>
+		</v-btn>
+		<v-btn icon @click="$router.push('help')">
+			<v-icon>mdi-help-circle-outline</v-icon>
+		</v-btn>
+		<v-btn icon @click="editDialog = true">
+			<v-icon>mdi-square-edit-outline</v-icon>
+		</v-btn>
+		<v-spacer></v-spacer>
+		<v-btn icon @click="closeZonescreen()">
+			<v-icon>mdi-close</v-icon>
+		</v-btn>
+	</v-toolbar>
+
+	<!-- ZONESCREEN ======================================================= -->
+	<v-layout column id="zonescreen">
+		<v-layout row class="zonerow" v-for="y in zonemap.height" :key="y">
+			<zm-sector
+				v-for="x in zonemap.width"
+				:key="coord(x, y)"
+				:id="coord(x, y)"
+				:gmeye="gmeye"
+				:sector="zonemap.get(coord(x, y))"
+				:selected="coord(x, y) === selectedCoord"
+				@click="selectedCoord = id"
+			/>
 		</v-layout>
-	</div>
+	</v-layout>
+
+	<!-- EDIT-SECTOR DIALOG =============================================== -->
+	<v-dialog
+		v-model="editDialog"
+		fullscreen
+		hide-overlay
+		transition="dialog-bottom-transition"
+	>
+		<v-card class="bg-edit-dialog">
+			<v-toolbar class="edit-dialog-toolbar" dark dense>
+				<v-btn icon dark @click="editDialog = false">
+					<v-icon>mdi-close</v-icon>
+				</v-btn>
+				<v-toolbar-title>Edit Sector: <b>{{ selectedSector.name }}</b></v-toolbar-title>
+				<v-spacer></v-spacer>
+				<v-toolbar-items>
+					<v-btn dark flat>
+						<v-icon>mdi-content-save</v-icon>&nbsp;Save
+					</v-btn>
+					<v-btn dark flat>
+						<v-icon>mdi-dice-6</v-icon>&nbsp;Roll
+					</v-btn>
+					<v-btn dark flat>
+						<v-icon>mdi-delete</v-icon>&nbsp;Delete
+					</v-btn>
+				</v-toolbar-items>
+			</v-toolbar>
+			<zm-edit-sector :sector="selectedSector"></zm-edit-sector>
+		</v-card>
+	</v-dialog>
+</div>
 </template>
 
 <script>
 import zmSector from '@/components/Sector.vue';
+import zmEditSector from '@/components/EditSector.vue';
 import ZoneMap from '@/zonemap/ZoneMap';
 import Util from '@/util/Util';
 
 export default {
 	name: 'zonescreen',
-	props: ['zonemap'],
+	props: {
+		zonemap: {
+			type: ZoneMap,
+			default: null
+		}
+	},
 	data: () => ({
 		alphabet: ['0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-		gmeye: true
+		gmeye: true,
+		selectedCoord: 'B12',
+		editDialog: true
 	}),
 	computed: {
 		mapTitle: function() {
@@ -46,6 +91,9 @@ export default {
 		},
 		sectors: function() {
 			return this.zonemap.array();
+		},
+		selectedSector: function() {
+			return this.zonemap.get(this.selectedCoord);
 		}
 	},
 	created: function() {
@@ -61,7 +109,8 @@ export default {
 		}
 	},
 	components: {
-		zmSector
+		zmSector,
+		zmEditSector
 	}
 }
 </script>
@@ -85,5 +134,20 @@ export default {
 .zonerow {
 	min-height: 64px;
 	max-height: 64px;
+}
+
+.bg-edit-dialog {
+	background-image: url('../assets/background.jpg');
+	background-repeat: no-repeat;
+	background-position: center center;
+	background-attachment: fixed;
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+}
+
+.edit-dialog-toolbar {
+	font-family: 'Futura Std Medium';
 }
 </style>
