a5f8cbc252b2806bf2e7e20b69b54a4a94f94a9b
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-03-24 02:39:55 +0100

    printWidth = 100

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,15 @@ module.exports = {
 	rules: {
 		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
 		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-		"prettier/prettier": ["error", { singleQuote: true, tabWidth: 4, useTabs: true }],
+		"prettier/prettier": [
+			"error",
+			{
+				singleQuote: true,
+				tabWidth: 4,
+				useTabs: true,
+				printWidth: 100
+			}
+		],
 	},
 	parserOptions: {
 		parser: "babel-eslint"
