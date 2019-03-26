7edaedc2ce7faf430d27de20e7f5731b6275e4b6
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-03-26 14:31:25 +0100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -5,12 +5,12 @@ import Home from '@/views/Home.vue';
 Vue.use(Router);
 
 export default new Router({
-	mode: 'history',
+	/* mode: 'history',
 	scrollBehavior: function(to, from, savedPosition) {
 		if (to.hash) return { selector: to.hash };
 		else if (savedPosition) return savedPosition;
 		return { x: 0, y: 0 };
-	},
+	}, */
 	routes: [
 		{
 			path: '/',
