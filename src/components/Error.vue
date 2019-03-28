ba437a984d7b80586a4516b876240551cdb0072e
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-03-28 17:42:35 +0100

    Initial commit

---
+++ src/components/Error.vue
@@ -0,0 +1,8 @@
+<template>
+<v-container fluid text-xs-center>
+	<v-layout row>
+		<v-icon>mdi-alert-circle-outline</v-icon>
+		<p>A fatal error occured!</p>
+	</v-layout>
+</v-container>
+</template>
---
+++ src/components/Loading.vue
@@ -0,0 +1,9 @@
+<template>
+<v-container text-xs-center fluid>
+	<v-progress-circular
+		indeterminate
+		color="deep-orange lighten-2"
+		size="100"
+	/>
+</v-container>
+</template>
