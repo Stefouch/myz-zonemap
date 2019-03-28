84ef498215458f86936c3ee39c223e7923026a14
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-03-28 17:44:29 +0100

    Added loading and error components

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home.vue';
+import LoadingComponent from '@/components/Loading.vue';
+import ErrorComponent from '@/components/Error.vue';
 
 Vue.use(Router);
 
@@ -22,7 +24,20 @@ export default new Router({
 			path: '/zonemap',
 			name: 'zonescreen',
 			// component: Zonescreen,
-			component: () => import('./views/Zonescreen.vue'),
+			// component: () => import('./views/Zonescreen.vue'),
+			component: () => ({
+				// The component to load (should be a Promise).
+				component: import('./views/Zonescreen.vue'),
+				// A component to use while the async component is loading.
+				loading: LoadingComponent,
+				// A component to use if the load fails.
+				error: ErrorComponent,
+				// Delay before showing the loading component. Default: 200ms.
+				delay: 200,
+				// The error component will be displayed if a timeout is
+				// provided and exceeded. Default: Infinity.
+				timeout: 30000
+			}),
 			props: true
 		},
 		{
