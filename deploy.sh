df3a40828491e3e18c534f76011d99ffc2a5de90
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-04-03 23:46:37 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Stefouch/zonemap.git master:gh-pages
+# git push -f git@github.com:Stefouch/zonemap.git master:gh-pages
+git subtree push --prefix dist origin gh-pages
 
 cd -
