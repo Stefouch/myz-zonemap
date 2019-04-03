88fc05127cd7260329ba36294ae852995ad2083e
Stefouch <47865544+Stefouch@users.noreply.github.com>
2019-04-04 01:00:17 +0200

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 @echo on
 REM https://gist.github.com/belohlavek/61dd16c08cd9c57a168408b9ac4121c2
 REM npm run build
-git init
+REM git init
 git add dist -f
 git commit -m 'deploy'
 git subtree push --prefix dist origin gh-pages
