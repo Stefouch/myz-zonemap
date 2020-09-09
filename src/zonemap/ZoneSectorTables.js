bdce4576f5c9bec7055efe473fba31252271e12f
Stefouch <47865544+Stefouch@users.noreply.github.com>
2020-09-09 09:54:39 +0200

    feat(Languages): Added German language

--- src/zonemap/ZoneSectorTables.js
+++ src/zonemap/ZoneSectorTables.js
@@ -2,6 +2,7 @@
  * List of sector-generator files. Available languages:
  * * EN: English
  * * FR: Français (French)
+ * * DE: Deutsche (German)
  * * SE: Sweden
  * * ES: Spanish
  * * PT: Portuguese
@@ -10,6 +11,7 @@
 module.exports = {
 	en: require('../data/sector-generator-en.json'),
 	fr: require('../data/sector-generator-fr.json'),
+	de: require('../data/sector-generator-de.json'),
 	se: require('../data/sector-generator-se.json'),
 	es: require('../data/sector-generator-es.json'),
 	pt: require('../data/sector-generator-pt.json'),
